f201a166dab8c51f9c688d29a42c63326e7cf911
AShvab <shvabovska@gmail.com>
2023-06-09 23:46:00 +0300

    work with images

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, Image } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+export default App = () => (
+  <View style={styles.container}>
+    <Text style={styles.title}>React Native</Text>
+       <Image
+  source={{ uri: 'https://cdn.pixabay.com/photo/2012/02/28/10/23/bee-18192_1280.jpg' }}
+  style={{ width: 200, height: 200 }}
+/>
+<Image
+  source={{ uri: 'https://cdn.pixabay.com/photo/2016/11/22/19/15/bee-1850116_1280.jpg' }}
+  style={{ width: 200, height: 200 }}
+/>
+<Image
+  source={{ uri: 'https://cdn.pixabay.com/photo/2018/04/29/18/01/bee-3360682_1280.jpg' }}
+  style={{ width: 200, height: 200 }}
+/>
+
+  </View>  
+);
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 24,
+    backgroundColor: "#eaeaea"
   },
+  title: {
+    marginTop: 16,
+    paddingVertical: 8,
+    borderWidth: 4,
+    borderColor: "#20232a",
+    borderRadius: 6,
+    backgroundColor: "#61dafb",
+    color: "#20232a",
+    textAlign: "center",
+    fontSize: 30,
+    fontWeight: "bold"
+  }
 });
